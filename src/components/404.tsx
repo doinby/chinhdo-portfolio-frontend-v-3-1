d483e0d8baa2327309b16cf88c6a1be55a586105
Chinh Do <chinh@doinby.co>
2023-06-10 20:01:38 +0700

    Refactor Loading & 404

---
+++ src/components/404.tsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+	return (
+		<section className='self-center'>
+			<img
+				src='/images/illustrations/404.svg'
+				alt='No Server Response'
+				className='h-96'
+			/>
+		</section>
+	);
+}
